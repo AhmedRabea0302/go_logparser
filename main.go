0aa3753a562210004df242e1eef7b2bfa404b3db
AhmedRabea0302 <arabea169@gmail.com>
2024-02-09 17:59:56 +0200

    Initialized project 🔥

--- main.go
+++ main.go
@@ -1,5 +1,35 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"regexp"
+	"strings"
+)
 
+func main() {
+	args := os.Args[1:]
+	in := bufio.NewScanner(os.Stdin)
+	in.Split(bufio.ScanWords)
+
+	query := args[0]
+	rx := regexp.MustCompile(`[^a-z]+`)
+
+	words := make(map[string]bool)
+	for in.Scan() {
+		word := strings.ToLower(in.Text())
+		word = rx.ReplaceAllString(word, "")
+
+		if len(word) > 2 {
+			words[word] = true
+		}
+	}
+
+	if words[query] {
+		fmt.Printf("The input contains %q.\n", query)
+		return
+	}
+
+	fmt.Printf("The input %q does not exist\n", query)
 }
