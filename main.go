8b03686a396aaa17a1e5c1b22ce50e406c078e92
AhmedRabea0302 <arabea169@gmail.com>
2024-02-10 15:28:52 +0200

    Enhanced using structs

--- main.go
+++ main.go
@@ -9,23 +9,29 @@ import (
 	"strings"
 )
 
-func main() {
-	var (
-		stats   map[string]int
-		domains []string
-		total   int
-		lines   int
-	)
+type result struct {
+	domain string
+	visits int
+}
+type parser struct {
+	stats   map[string]result // Total visits per domain
+	domains []string          // Unique domains
+	total   int               // Total visits for all domains
+	lines   int               // Number of parse lines
+}
 
+func main() {
 	in := bufio.NewScanner(os.Stdin)
-	stats = make(map[string]int)
+	p := parser{
+		stats: make(map[string]result),
+	}
 
 	for in.Scan() {
-		lines++
+		p.lines++
 
 		fields := strings.Fields(in.Text())
 		if len(fields) < 2 {
-			fmt.Printf("Wrong Input: %v (line #%d)\n", fields, lines)
+			fmt.Printf("Wrong Input: %v (line #%d)\n", fields, p.lines)
 			return
 		}
 
@@ -33,26 +39,29 @@ func main() {
 		visits, err := strconv.Atoi(fields[1])
 
 		if visits < 0 || err != nil {
-			fmt.Printf("Wrong Input: %v (line #%d)\n", fields[1], lines)
+			fmt.Printf("Wrong Input: %v (line #%d)\n", fields[1], p.lines)
 			return
 		}
 
-		total += visits
-		if _, ok := stats[domain]; !ok {
-			domains = append(domains, domain)
+		p.total += visits
+		if _, ok := p.stats[domain]; !ok {
+			p.domains = append(p.domains, domain)
 		}
-		stats[domain] += visits
+		p.stats[domain] = result{
+			domain: domain,
+			visits: visits + p.stats[domain].visits,
+		}
 	}
 
 	fmt.Printf("%-30s %10s\n", "DOMAIN", "VISITS")
 	fmt.Println(strings.Repeat("-", 45))
 
-	sort.Strings(domains)
-	for _, domain := range domains {
-		visits := stats[domain]
-		fmt.Printf("%-30s %10d\n", domain, visits)
+	sort.Strings(p.domains)
+	for _, domain := range p.domains {
+		parsed := p.stats[domain]
+		fmt.Printf("%-30s %10d\n", domain, parsed.visits)
 	}
-	fmt.Printf("\n%-30s %10d\n", "TOTAL", total)
+	fmt.Printf("\n%-30s %10d\n", "TOTAL", p.total)
 
 	if err := in.Err(); err != nil {
 		fmt.Printf("Error: %v\n", err)
